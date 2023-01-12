df244b50fc84b22391c9c5701115b908cefd5970
Ivan <ivanbar8112003@gmail.com>
2023-01-12 17:25:37 -0300

    Display main idea

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [
-  {title: "titulo", author:"Yo", pages:123, isRead:true}
+  {title: "titulo", author:"Yo", pages:123, isRead:true},
+  {title: "titulazo", author:"El", pages:2, isRead:false},
+  {title: "UFFF", author:"alla", pages:1233, isRead:true},
+  {title: "CACA", author:"nomolestes", pages:444, isRead:true}
 ];
 
 function Book(title, author, pages, isRead) {
@@ -9,6 +12,9 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead
 }
 
+const booksDisplay = document.querySelector('.books-display');
+
+
 function getBookFromInput() {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
@@ -17,13 +23,45 @@ function getBookFromInput() {
   return new Book(title,author,pages,isRead);
 }
 
-function addBookToLibrary(e) {
-  e.preventDefault();
+function addBookToLibrary() {
   const newBook = getBookFromInput();
   myLibrary.push(newBook);
   console.log(myLibrary);
 }
 
-function displayBooks() {
 
+function createBookCard(book){
+  const container = document.createElement('div');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const isRead = document.createElement('button');
+
+  container.classList.add('card-container');
+  isRead.classList.add('read-btn');
+
+  title.textContent = `Title: ${book.title}`;
+  author.textContent = `Author: ${book.author}`;
+  pages.textContent = `Pages: ${book.pages}`;
+  
+  if (book.isRead) {
+    isRead.textContent = 'Read';
+  }
+  else{
+    isRead.textContent = 'Not Read';
+  }
+
+  container.appendChild(title);
+  container.appendChild(author);
+  container.appendChild(pages);
+  container.appendChild(isRead);
+  booksDisplay.appendChild(container);
 }
+
+function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBookCard(myLibrary[i]);
+  }
+}
+console.log(myLibrary.length);
+displayBooks();
